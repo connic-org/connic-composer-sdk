7beae486121ab1da3a63808f51d03d0dacbf3b1e
connic <info@connic.co>
2026-03-20 01:23:04 +0100

    Bumps project version to 0.1.9

--- src/connic/__init__.py
+++ src/connic/__init__.py
@@ -32,7 +32,7 @@ from . import tools
 from .core import Agent, AgentConfig, GuardrailResult, Middleware, RetryOptions, StopProcessing, Tool
 from .loader import ProjectLoader
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
 __all__ = [
     "Agent",
